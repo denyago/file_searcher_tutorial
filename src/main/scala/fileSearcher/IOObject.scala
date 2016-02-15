e0ae79cf6e9a01657bee954ab5959e166d978f2a
Denys Yahofarov <denyago@gmail.com>
2016-02-15 19:01:37 +0100

    - unneeded brackets

--- src/main/scala/fileSearcher/IOObject.scala
+++ src/main/scala/fileSearcher/IOObject.scala
@@ -1,11 +1,8 @@
 package fileSearcher
 
-/**
-  * Created by di on 15/02/2016.
-  */
 trait IOObject {
   val name: String
 }
 
-class FileObject(val name: String) extends IOObject {}
-class DirectoryObject(val name: String) extends IOObject {}
+class FileObject(val name: String) extends IOObject
+class DirectoryObject(val name: String) extends IOObject
--- src/test/scala/fileSearcher/FilterCheckerTests.scala
+++ src/test/scala/fileSearcher/FilterCheckerTests.scala
@@ -7,14 +7,14 @@ class FilterCheckerTests extends FlatSpec {
   "return a list with that file" in {
     val matchingFile = new FileObject("match")
     val listOfFiles  = List(new FileObject("random"), matchingFile)
-    val matchedFiles = new FilterChecker("match").findMatchedFiles(listOfFiles)
+    val matchedFiles = FilterChecker("match") findMatchedFiles listOfFiles
     assert(matchedFiles == List(matchingFile))
   }
 
   "FilterChecker passed a list whith a directory that matches the filter" should
   "not return the directory" in {
     val listOfObjects = List(new FileObject("random"), new DirectoryObject("match"))
-    val matchedFiles  = new FilterChecker("match").findMatchedFiles(listOfObjects)
+    val matchedFiles  = FilterChecker("match") findMatchedFiles listOfObjects
     assert(matchedFiles.length == 0)
   }
 }
