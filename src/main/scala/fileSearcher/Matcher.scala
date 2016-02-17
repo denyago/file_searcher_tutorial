475c86e1cbf30e4188098f7dc8d4415576f25325
Denys Yahofarov <denyago@gmail.com>
2016-02-17 11:19:59 +0100

    + content filtering

--- src/main/scala/fileSearcher/Matcher.scala
+++ src/main/scala/fileSearcher/Matcher.scala
@@ -3,24 +3,33 @@ package fileSearcher
 import java.io.File
 import scala.annotation.tailrec
 
-class Matcher(filter: String, val rootLocation: String = new File(".").getCanonicalPath) {
+class Matcher(filter: String, val rootLocation: String = new File(".").getCanonicalPath,
+              contentFilter: Option[String] = None) {
   val rootIOObject = FileConverter.convertToIOObject(new File(rootLocation))
 
   def execute(): List[String] = {
 
     @tailrec
-    def recurciveMatch(files: List[IOObject], matches: List[FileObject]): List[FileObject] = {
+    def nameMatch(files: List[IOObject], matches: List[FileObject]): List[FileObject] = {
       files match {
         case List()           => matches
         case iOObject :: rest =>
           iOObject match {
-            case file      : FileObject if FilterChecker(filter).matches(file.name) => recurciveMatch(rest, file :: matches)
-            case directory : DirectoryObject => recurciveMatch(rest ::: directory.children(), matches)
-            case _         => recurciveMatch(rest, matches)
+            case file      : FileObject if FilterChecker(filter).matches(file.name) => nameMatch(rest, file :: matches)
+            case directory : DirectoryObject => nameMatch(rest ::: directory.children(), matches)
+            case _         => nameMatch(rest, matches)
           }
       }
     }
 
-    recurciveMatch(List(rootIOObject), List()).map(iOObject => iOObject.name)
+    def contentMatch(files: List[FileObject]) = {
+      contentFilter match {
+        case Some(dataFilter) => files.filter(iOObject => FilterChecker(dataFilter).matchesFileContent(iOObject.file))
+        case None             => files
+      }
+    }
+
+
+    contentMatch(nameMatch(List(rootIOObject), List())).map(iOObject => iOObject.name)
   }
 }
--- src/test/scala/fileSearcher/MatcherTests.scala
+++ src/test/scala/fileSearcher/MatcherTests.scala
@@ -5,6 +5,8 @@ import java.io.File
 import org.scalatest.FlatSpec
 
 class MatcherTests extends FlatSpec{
+  val testFilesDirPath = new File("src/test/resources/testfiles").getCanonicalPath
+
   "Matcher that is passed a file matching the filter" should
   "return a list with that file name" in {
     val matcher = new Matcher("fake", "fakePath")
@@ -14,7 +16,7 @@ class MatcherTests extends FlatSpec{
 
   "Matcher using a directory containing one file matching the filter" should
   "return a list with that file name" in {
-    val matcher = new Matcher("readme", new File("src/test/resources/testfiles").getCanonicalPath)
+    val matcher = new Matcher("readme", testFilesDirPath)
     val results = matcher.execute()
     assert(results == List("readme.txt"))
   }
@@ -27,8 +29,22 @@ class MatcherTests extends FlatSpec{
 
   "Matcher with sub folder checking matching a root location with N subtree files matching" should
   "return a list with those file names" in {
-    val matcher = new Matcher("txt", new File("src/test/resources/testfiles").getCanonicalPath)
+    val matcher = new Matcher("txt", testFilesDirPath)
     val results = matcher.execute()
     assert(results == List("notes.txt", "readme.txt"))
+  }
+
+  "Matcher given a path that has one file that matches file filter and content filter" should
+  "return a list with that file name" in {
+    val matcher = new Matcher("data", testFilesDirPath, Some("content test"))
+    val results = matcher.execute()
+    assert(results == List("content_tests.data"))
+  }
+
+  "Matcher given a path that has no files that matches file filter and content filter" should
+    "return an empty list" in {
+    val matcher = new Matcher("txt", testFilesDirPath, Some("content test"))
+    val results = matcher.execute()
+    assert(results == List())
   }
 }
